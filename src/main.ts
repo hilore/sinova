138b9aaf1eb907fdbaa491b8de6cc101acd94dcb
Mark Parker <marksadpepe@gmail.com>
2024-08-19 21:59:12 +0300

    minor changes

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 }
 
 bootstrap();
