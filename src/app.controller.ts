0d67728b1c74ee860860d1a0af8ec4d03a1c2638
Mark Parker <marksadpepe@gmail.com>
2024-08-19 16:08:21 +0300

    added Swagger documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ApiTags, ApiOperation, ApiResponse} from "@nestjs/swagger";
 
+@ApiTags("ping")
 @Controller("/ping")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({summary: "Pings server"})
+  @ApiResponse({
+    status: 200,
+    description: "The server responds to the ping"
+  })
+  @ApiResponse({
+    status: 429,
+    description: "Rate limit exceeded"
+  })
+  @ApiResponse({
+    status: 500,
+    description: "Internal Server Error"
+  })
   getPong(): string {
     return this.appService.getPong();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
 
 async function bootstrap() {
   const port = parseInt(process.env.BACKEND_PORT, 10) || 4000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Sinova Test Assignment Rest API Documentation")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(port);
 }
